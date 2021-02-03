da89652d1bf1b78ee994c9757bc0a2a7c6da2fe7
Maximys717 <ahamambar1911@gmail.com>
2021-02-03 21:22:36 +0200

    Update unblock_setup.sh

--- unblock-list/unblock-scripts/unblock_setup.sh
+++ unblock-list/unblock-scripts/unblock_setup.sh
@@ -6,27 +6,6 @@ chmod +x /etc/storage/unblock/unblock_setup.sh
 
 
 ### init ipset
-echo '' >> /etc/storage/start_script.sh
-echo '### TOR. Example - load ipset modules.' >> /etc/storage/start_script.sh
-echo 'modprobe ip_set' >> /etc/storage/start_script.sh
-echo 'modprobe ip_set_hash_ip' >> /etc/storage/start_script.sh
-echo 'modprobe ip_set_hash_ip' >> /etc/storage/start_script.sh
-echo 'modprobe ip_set_hash_net #main' >> /etc/storage/start_script.sh
-echo 'modprobe ip_set_bitmap_ip' >> /etc/storage/start_script.sh
-echo 'modprobe ip_set_list_set' >> /etc/storage/start_script.sh
-echo 'modprobe xt_set #main' >> /etc/storage/start_script.sh
-echo '' >> /etc/storage/start_script.sh
-echo '### TOR. creating of unblock array' >> /etc/storage/start_script.sh
-echo 'ipset create unblock hash:net #main' >> /etc/storage/start_script.sh
-echo '' >> /etc/storage/start_script.sh
-echo '' >> /etc/storage/start_script.sh
-
-echo '### comments.some scripts to remember' >> /etc/storage/start_script.sh
-echo '# /etc/storage/unblock/unblock.sh #main script.setup-start-update' >> /etc/storage/start_script.sh
-echo '# /etc/storage/unblock/unblock_update.sh #main script.start-Update' >> /etc/storage/start_script.sh
-echo '# ipset list unblock #check unblock-ipset array' >> /etc/storage/start_script.sh
-echo '' >> /etc/storage/start_script.sh
-
 echo '
 ### TOR. Example - load ipset modules.
 modprobe ip_set
@@ -45,21 +24,5 @@ ipset create unblock hash:net #main
 # ipset list unblock #check unblock-ipset array
 
 ' >> /etc/storage/start_script.sh
-
-
-#unblock_Setup
-echo Download List
-wget -q -O /etc/storage/unblock/unblock.txt https://raw.githubusercontent.com/Maximys717/greentea/master/unblock-list/unblock.txt
-
-
-echo Download scripts
-echo downloading unblock_ipset.sh
-wget -q -O /etc/storage/unblock/unblock_ipset.sh https://raw.githubusercontent.com/Maximys717/greentea/master/unblock-list/unblock-scripts/unblock_ipset.sh
-chmod +x /etc/storage/unblock/unblock_ipset.sh
-
-echo downloading unblock_update.sh
-wget -q -O /etc/storage/unblock/unblock_update.sh https://raw.githubusercontent.com/Maximys717/greentea/master/unblock-list/unblock-scripts/unblock_update.sh
-chmod +x /etc/storage/unblock/unblock_update.sh
-
 
 echo Finish.
