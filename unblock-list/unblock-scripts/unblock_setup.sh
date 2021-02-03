df230f52d922b9efb2f179934305fd1903fce2cb
Maximys717 <ahamambar1911@gmail.com>
2021-02-03 21:26:33 +0200

    Update unblock_setup.sh

--- unblock-list/unblock-scripts/unblock_setup.sh
+++ unblock-list/unblock-scripts/unblock_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p /etc/storage/unblock
-wget -q -O /etc/storage/unblock/unblock_setup.sh https://raw.githubusercontent.com/Maximys717/greentea/master/unblock-list/unblock-scripts/unblock_setup.sh
-chmod +x /etc/storage/unblock/unblock_setup.sh
+#mkdir -p /etc/storage/unblock
+#wget -q -O /etc/storage/unblock/unblock_setup.sh https://raw.githubusercontent.com/Maximys717/greentea/master/unblock-list/unblock-scripts/unblock_setup.sh
+#chmod +x /etc/storage/unblock/unblock_setup.sh
 
 
 ### init ipset
