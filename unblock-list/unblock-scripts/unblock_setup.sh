93be8e220ef53900d2b911b13cf9a7210fd4de5e
Maximys717 <ahamambar1911@gmail.com>
2021-02-03 21:55:10 +0200

    Update unblock_setup.sh

--- unblock-list/unblock-scripts/unblock_setup.sh
+++ unblock-list/unblock-scripts/unblock_setup.sh
@@ -6,23 +6,6 @@
 
 
 ### init ipset
-echo '
-### TOR. Example - load ipset modules.
-modprobe ip_set
-modprobe ip_set_hash_ip
-modprobe ip_set_hash_net #main
-modprobe ip_set_bitmap_ip
-modprobe ip_set_list_set
-modprobe xt_set #main
 
-### TOR. creating of unblock array
-ipset create unblock hash:net #main
-
-### comments.some scripts to remember
-# /etc/storage/unblock/unblock.sh #main script.setup-start-update
-# /etc/storage/unblock/unblock_update.sh #main script.start-Update
-# ipset list unblock #check unblock-ipset array
-
-' >> /etc/storage/start_script.sh
 
 echo Finish.
