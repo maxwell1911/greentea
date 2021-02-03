6b183a30ab55eac639a9625e23ac8b751a9f3fc8
Maximys717 <ahamambar1911@gmail.com>
2021-02-03 23:12:51 +0200

    Update unblock_setup.sh

--- unblock-list/unblock-scripts/unblock_setup.sh
+++ unblock-list/unblock-scripts/unblock_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#unblock_Setup
+echo Download List
+
 #mkdir -p /etc/storage/unblock
 #wget -q -O /etc/storage/unblock/unblock_setup.sh https://raw.githubusercontent.com/Maximys717/greentea/master/unblock-list/unblock-scripts/unblock_setup.sh
 #chmod +x /etc/storage/unblock/unblock_setup.sh
